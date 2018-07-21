6a7f32d39208b3a20a561df778a37aab808c580d
凌陆达 <759689674@qq.com>
2018-07-21 12:40:20 +0800

    this域问题！

--- src/components/base/navbar.vue
+++ src/components/base/navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <Menu mode="horizontal" :theme="'dark'" active-name="1" style="hight=100vh;float:right" @on-select="changeMenu">
+  <Menu mode="horizontal" theme="dark" active-name="1" style="hight:100vh;float:right" @on-select="changeMenu">
     <MenuItem name="1">
       <Icon type="ios-paper"></Icon>
       内容管理
@@ -30,54 +30,14 @@
   export default {
     data() {
       return {
-        theme1: "dark",
-        menu: [],
+
       };
     },
     methods: {
       changeMenu(active) {
-        if (active === "1") {
-        this.menu = [
-          {
-            id: 1,
-            name: "消息推送2",
-            path: "",
-            type: "ios-home",
-            childen: [
-              {
-                id: 5,
-                name: "用户管理2",
-                path: "/msgsend",
-                type: ""
-              }
-            ]
-          }
-        ];
-        this.bus.$emit('toChangeTitle', this.menu);
-        console.log('navbar:', this.menu)        
-      }
-       if (active === "2") {
-           this.menu = [
-          {
-            id: 2,
-            name: "消息推送2",
-            path: "",
-            type: "ios-home",
-            childen: [
-              {
-                id: 6,
-                name: "用户管理2",
-                path: "/msgsend",
-                type: ""
-              }
-            ]
-          }
-        ];
-        this.bus.$emit('toChangeTitle', this.menu);
+        this.bus.$emit('toChangeTitle', active);
         console.log('navbar:', this.menu)
-       }
-        // this.bus.$emit('toChangeTitle', active);
-        // console.log('navbar:', active)
+
       }
     }
   };
